4b5f3ae8c05735139e42aa3c1aceb4e05bee1fb2
Saim Hafeez <saimhafeez2020@gmail.com>
2023-12-04 23:10:56 +0500

    User Management From Admin Page admin can reassign roles and activate or deactivate accounts.

--- src/pages/admin/UserManagement.js
+++ src/pages/admin/UserManagement.js
@@ -2,15 +2,19 @@ import React from 'react'
 import HeaderSignOut from '../../components/header/HeaderSignOut'
 import SuperAdminSidebar from '../../components/sidebar/SuperAdminSidebar'
 import { firestore } from '../../firebase';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Stack, Typography, Button } from '@mui/material';
 
 function UserManagement(props) {
 
     const sortOrderForJobdesc = ["Extractor", "QA-Extractor", "DimAna", "QA-DimAna"];
-    const sortOrderForRole = ["manager", "worker"];
+
+    const [filter, setFilter] = useState({
+        status: 'active',
+        isEditingRole: false
+    })
 
 
     const [userList, setUserList] = useState({
@@ -18,6 +22,9 @@ function UserManagement(props) {
         workers: [],
         managers: []
     })
+
+    const [workersRoleUpdated, setWorkersRoleUpdated] = useState({})
+    const [managersRoleUpdated, setManagersRoleUpdated] = useState({})
 
 
     const getAllUsers = async () => {
@@ -30,7 +37,7 @@ function UserManagement(props) {
         const usersCollectionRef = collection(firestore, "users");
 
         // Use a query to filter users with role=manager
-        const q = query(usersCollectionRef, where("role", "==", "worker"), where("role", "==", "manager"));
+        const q = query(usersCollectionRef);
 
         // Fetch data based on the query
         const snapshot = await getDocs(q);
@@ -42,31 +49,88 @@ function UserManagement(props) {
 
         console.log('users', users);
 
-        const sortedWorkers = users.sort((a, b) => {
+        const sortedWorkers = users.filter((user) => user.role === 'worker' && user.status === filter.status).sort((a, b) => {
             const indexA = sortOrderForJobdesc.indexOf(a.jdesc);
             const indexB = sortOrderForJobdesc.indexOf(b.jdesc);
             return indexA - indexB;
         })
 
-        const sortedManagers = users.sort((a, b) => {
-            const indexA = sortOrderForRole.indexOf(a.jdesc);
-            const indexB = sortOrderForRole.indexOf(b.jdesc);
+        const sortedManagers = users.filter((user) => user.role === 'manager' && user.status === filter.status).sort((a, b) => {
+            const indexA = sortOrderForJobdesc.indexOf(a.jdesc);
+            const indexB = sortOrderForJobdesc.indexOf(b.jdesc);
             return indexA - indexB;
         })
 
         console.log('sorted', sortedManagers);
 
-        setUserList({
+        setUserList(pre => ({
+            ...pre,
             isLoading: false,
             workers: sortedWorkers,
             managers: sortedManagers
-        })
+        }))
 
     }
 
     useEffect(() => {
         getAllUsers()
-    }, [])
+    }, [filter.status])
+
+
+    const AddUserForUpdation = (user, newRole) => {
+
+        console.log('user', user);
+
+        if (user.role === 'manager') {
+            userList.managers.map((_user, index) => {
+                if (_user.id === user.id && _user.jdesc !== newRole) {
+                    setManagersRoleUpdated(pre => ({
+                        ...pre,
+                        [user.id]: newRole
+                    }))
+                }
+            })
+        } else {
+            userList.workers.map((_user, index) => {
+                if (_user.id === user.id && _user.jdesc !== newRole) {
+                    setWorkersRoleUpdated(pre => ({
+                        ...pre,
+                        [user.id]: newRole
+                    }))
+                }
+            })
+        }
+
+    }
+
+    const updateUserStatus = async (docId, newStatus) => {
+        console.log(docId, newStatus);
+        const usersCollectionRef = collection(firestore, "users");
+        const userDocRef = doc(usersCollectionRef, docId);
+        // Update the jdesc field with the new value
+        await updateDoc(userDocRef, { status: newStatus });
+        getAllUsers()
+    }
+
+    const handleUpdateUserRoles = async () => {
+        const usersCollectionRef = collection(firestore, "users");
+
+        for (const [docId, newJdesc] of Object.entries(managersRoleUpdated)) {
+            const userDocRef = doc(usersCollectionRef, docId);
+
+            // Update the jdesc field with the new value
+            await updateDoc(userDocRef, { jdesc: newJdesc });
+        }
+
+        for (const [docId, newJdesc] of Object.entries(workersRoleUpdated)) {
+            const userDocRef = doc(usersCollectionRef, docId);
+
+            // Update the jdesc field with the new value
+            await updateDoc(userDocRef, { jdesc: newJdesc });
+        }
+
+        getAllUsers()
+    };
 
     return (
         <>
@@ -80,55 +144,163 @@ function UserManagement(props) {
 
             <div className="set-right-container-252 p-3" style={{ height: 'calc(100vh - 70px)', overflow: 'auto' }}>
 
+                <Stack direction='row' justifyContent='center'>
+                    <Stack direction='row' margin='10px' gap={1}>
+                        <Button
+                            onClick={() => setFilter(pre => ({
+                                ...pre,
+                                status: 'active'
+                            }))}
+                            variant={filter.status === 'active' ? "contained" : "outlined"}
+
+                        >
+                            <Typography>Active</Typography>
+                        </Button>
+                        <Button
+                            onClick={() => setFilter(pre => ({
+                                ...pre,
+                                status: 'inactive'
+                            }))}
+                            variant={filter.status === 'inactive' ? "contained" : "outlined"}
+                        >
+                            <Typography>Inactive</Typography>
+                        </Button>
+                    </Stack>
+                </Stack>
+
                 {userList.isLoading ? <CircularProgress size={56} color="info" /> : <table className="table mt-4 table-bordered table-striped align-middle text-center">
                     <thead className="table-dark">
-                        <tr className="border-0 bg-white">
-                            <th colSpan={2} className="bg-white text-dark border-0">
-                                Results Found
-                            </th>
-                            <th className="bg-white" style={{ maxWidth: 150 }}>
-                                <div className="d-flex flex-row">
-                                    <input
-                                        className="p-2 w-100"
-                                        type="text"
-                                        placeholder="Search by ProductID"
-                                        style={{ backgroundColor: "#e8e8e8", width: "fit-content" }}
-                                        onChange={(e) => { }}
-                                        value=''
-                                    />
-                                    <button className="btn btn-go-fetch" onClick={() => { }}>Clear</button>
-                                </div>
-                            </th>
-                            <th className="bg-white"></th>
-                            <th className="bg-white"></th>
-                            <th className="bg-white">
-
-                                <select
-                                    className="p-2 w-100"
-                                    name="qa-status"
-                                    id="qa-status"
-                                    onChange={(e) => { }}
-                                    value={''}
-                                >
-                                    <option value="qa-status">Filter by QA Status</option>
-                                    <option value="under_qa">Under QA</option>
-                                    <option value="passed">100% [QA Passed]</option>
-                                    <option value="minor">MINOR[QA Passed]</option>
-                                    <option value="major">MAJOR [QA Passed]</option>
-                                </select>
-
-                            </th>
+                        <tr>
+                            <td colSpan={5} style={{ backgroundColor: '#ffca28', color: 'black' }}>
+                                <h3 className='fw-bold'>Managers</h3>
+                            </td>
                         </tr>
                         <tr>
                             <th># SR</th>
                             <th>Name</th>
-                            {/* <th>Earning</th> */}
+                            <th>Email</th>
+                            <th className='d-flex justify-content-between align-items-center'>
+                                <div></div>
+                                <span>Role</span>
+                                <button className='btn btn-outline-light d-flex justify-content-center gap-1 align-items-center' style={{ cursor: 'pointer' }} onClick={() => {
+                                    if (filter.isEditingRole) {
+                                        console.log('saved', managersRoleUpdated);
+                                        handleUpdateUserRoles()
+                                    }
+                                    setFilter(pre => ({
+                                        ...pre,
+                                        isEditingRole: !pre.isEditingRole
+                                    }))
+                                }}>
+                                    <span className='small'>
+                                        {!filter.isEditingRole ? 'Edit?' : 'Save'}
+                                    </span>
+                                </button>
+                            </th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {getAllUsers().map((item, index) => (
+                        {userList.managers.length === 0 && <tr >
+                            <td colSpan={5}>
+                                Empty Results
+                            </td>
+                        </tr>}
+                        {userList.managers.map((item, index) => (
                             <tr key={index}>
+                                <td>{index + 1}</td>
                                 <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>
+                                    <select
+                                        value={managersRoleUpdated[item.id] || item.jdesc}
+                                        disabled={!filter.isEditingRole}
+                                        onChange={(e) => AddUserForUpdation(item, e.currentTarget.value)}
+                                    >
+                                        <option value='Extractor'>Extractor</option>
+                                        <option value='QA-Extractor'>QA-Extractor</option>
+                                        <option value='DimAna'>DimAna</option>
+                                        <option value='QA-DimAna'>QA-DimAna</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    {
+                                        <button className='btn btn-fetch btn-go-fetch'
+                                            onClick={() => updateUserStatus(item.id, filter.status === 'active' ? 'inactive' : 'active')}
+                                        >
+                                            {filter.status === 'active' ? 'Deactivate Account' : 'Activate Account'}
+                                        </button>
+                                    }
+
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                    <thead className="table-dark">
+                        <tr>
+                            <td colSpan={5} style={{ backgroundColor: '#ffca28', color: 'black' }}>
+                                <h3 className='fw-bold'>Workers</h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th># SR</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th className='d-flex justify-content-between align-items-center'>
+                                <div></div>
+                                <span>Role</span>
+                                <button className='btn btn-outline-light d-flex justify-content-center gap-1 align-items-center' style={{ cursor: 'pointer' }} onClick={() => {
+                                    if (filter.isEditingRole) {
+                                        console.log('saved', workersRoleUpdated);
+                                        handleUpdateUserRoles()
+                                    }
+                                    setFilter(pre => ({
+                                        ...pre,
+                                        isEditingRole: !pre.isEditingRole
+                                    }))
+                                }}>
+                                    <span className='small'>
+                                        {!filter.isEditingRole ? 'Edit?' : 'Save'}
+                                    </span>
+                                </button>
+                            </th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userList.workers.length === 0 && <tr >
+                            <td colSpan={5}>
+                                Empty Results
+                            </td>
+                        </tr>}
+                        {userList.workers.map((item, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>
+                                    <select
+                                        value={workersRoleUpdated[item.id] || item.jdesc}
+                                        disabled={!filter.isEditingRole}
+                                        onChange={(e) => AddUserForUpdation(item, e.currentTarget.value)}
+                                    >
+                                        <option value='Extractor'>Extractor</option>
+                                        <option value='QA-Extractor'>QA-Extractor</option>
+                                        <option value='DimAna'>DimAna</option>
+                                        <option value='QA-DimAna'>QA-DimAna</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    {
+                                        <button className='btn btn-fetch btn-go-fetch'
+                                            onClick={() => updateUserStatus(item.id, filter.status === 'active' ? 'inactive' : 'active')}
+                                        >
+                                            {filter.status === 'active' ? 'Deactivate Account' : 'Activate Account'}
+                                        </button>
+                                    }
+
+                                </td>
                             </tr>
                         ))}
                     </tbody>
